62fee32f658c2348b2673f7ff3b154d0d690c093
David Smith <dbsmith.dbs83@gmail.com>
2016-08-23 13:19:07 -0700

    class demo changes

--- website/views.py
+++ website/views.py
@@ -1,33 +1,41 @@
 from pyramid.view import view_config
-from pyramid.response import Response
 import os
 
 HERE = os.path.dirname(__file__)
+ENTRIES = [
+    {
+        "title": "This is number 1",
+        "id": 1,
+        "date": "August 23"
+        "body": "blah blah"
+    },
+    {
+        "title": "This is number 2",
+        "id": 2,
+        "date": "August 24"
+        "body": "blah blah"
+    },
+]
 
-
+@view_config(route_name='home', renderer='templates/detail.jinja2')
 def home_page(request):
     imported_text = open(os.path.join(HERE, 'templates/index.html')).read()
-    return Response(imported_text)
+    return imported_text
 
 
+@view_config(route_name='detail', renderer='templates/detail.jinja2')
 def detail(request):
     imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
-    return Response(imported_text)
+    return imported_text
 
 
-@view_config(route_name='edit')
+@view_config(route_name='edit', renderer='templates/detail.jinja2')
 def edit(request):
     imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
-    return Response(imported_text)
+    return imported_text
 
 
+@view_config(route_name='new', renderer='templates/detail.jinja2')
 def new(request):
     imported_text = open(os.path.join(HERE, 'templates/new.html')).read()
-    return Response(imported_text)
-
-
-def includeme(config):
-    config.add_view(home_page, route_name='home')
-    config.add_view(detail, route_name='detail')
-    config.add_view(edit, route_name='edit')
-    config.add_view(new, route_name='new')
+    return imported_text
