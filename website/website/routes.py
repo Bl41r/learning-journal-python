ca3434a143ef6188bdf61dd261bbefe6cef92da6
David Smith <dbsmith.dbs83@gmail.com>
2016-08-23 00:02:34 -0700

    added routes not showing up

--- website/website/routes.py
+++ website/website/routes.py
@@ -1,6 +1,6 @@
 def includeme(config):
     config.add_route('home', '/')
-    config.add_route('detail', '/detail/')
-    config.add_route('edit', '/edit/')
-    config.add_route('new', '/new/')
+    config.add_route('detail', '/journal/{id:\d+}')
+    config.add_route('edit', '/journal/new-entry')
+    config.add_route('new', '/journal/{id:\d+}/edit-entry')
     config.add_static_view('static', 'static', cache_max_age=3600)
--- website/website/views.py
+++ website/website/views.py
@@ -9,5 +9,23 @@ def home_page(request):
     return Response(imported_text)
 
 
+def detail(request):
+    imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
+    return Response(imported_text)
+
+
+def edit(request):
+    imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
+    return Response(imported_text)
+
+
+def new(request):
+    imported_text = open(os.path.join(HERE, 'templates/new.html')).read()
+    return Response(imported_text)
+
+
 def includeme(config):
     config.add_view(home_page, route_name='home')
+    config.add_view(detail, route_name='detail')
+    config.add_view(edit, route_name='edit')
+    config.add_view(new, route_name='new')
